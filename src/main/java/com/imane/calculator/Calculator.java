e7e9fe85e93eab2fd69964c4f7a5391f0a48f7bf
ImaneTech <imane.zouine100@gmail.com>
2024-12-10 12:09:05 +0100

    Ajout de la méthode add pour effectuer une addition

--- src/main/java/com/imane/calculator/Calculator.java
+++ src/main/java/com/imane/calculator/Calculator.java
@@ -1,4 +1,12 @@
 package com.imane.calculator;
 
 public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+        public int divide(int a, int b) {
+            return a / b;
+        }
 }
+
+
--- src/test/java/com/imane/calculator/CalculatorTest.java
+++ src/test/java/com/imane/calculator/CalculatorTest.java
@@ -1,4 +1,18 @@
 package com.imane.calculator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-}
+        @Test
+        public void testAddition() {
+            Calculator calculator = new Calculator();
+            int result = calculator.add(2, 3);
+            assertEquals(5, result, "2 + 3 doit être égal à 5");
+        }
+     @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        int result = calculator.divide(15, 3);
+        assertEquals(5, result, "15 / 3 doit être égal à 5");
+    }
+    }
