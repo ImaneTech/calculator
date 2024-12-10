829d6754452e339bbc2da2dfc00aad7e95213a95
ImaneTech <imane.zouine100@gmail.com>
2024-12-10 15:48:15 +0100

    Question 7:Simplification d’un test : méthodes de montage / démontage

--- src/test/java/com/imane/calculator/CalculatorTest.java
+++ src/test/java/com/imane/calculator/CalculatorTest.java
@@ -1,18 +1,24 @@
 package com.imane.calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 public class CalculatorTest {
-        @Test
+    private Calculator calculator;
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();}
+    @AfterEach
+    void tearDown() {
+        calculator = null;}
+     @Test
         public void testAddition() {
             Calculator calculator = new Calculator();
             int result = calculator.add(2, 3);
-            assertEquals(5, result, "2 + 3 doit être égal à 5");
-        }
+            assertEquals(5, result, "2 + 3 doit être égal à 5");}
      @Test
-    public void testDivide() {
+       public void testDivide() {
         Calculator calculator = new Calculator();
         int result = calculator.divide(15, 3);
-        assertEquals(5, result, "15 / 3 doit être égal à 5");
-    }
+        assertEquals(5, result, "15 / 3 doit être égal à 5");}
     }
