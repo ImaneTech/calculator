bcd86c63de72d17553097a2fdccf3f5ae773e7f7
ImaneTech <imane.zouine100@gmail.com>
2024-12-10 16:29:48 +0100

    Question 9:Écrire des tests paramétrés

--- src/main/java/com/imane/calculator/Calculator.java
+++ src/main/java/com/imane/calculator/Calculator.java
@@ -1,7 +1,7 @@
 package com.imane.calculator;
 
 public class Calculator {
-        public int add(int a, int b) {
+        public static int add(int a, int b) {
             return a + b;
         }
         public int divide(int a, int b) {
--- src/test/java/com/imane/calculator/CalculatorTest.java
+++ src/test/java/com/imane/calculator/CalculatorTest.java
@@ -3,6 +3,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
     private Calculator calculator;
     @BeforeEach
@@ -21,4 +25,22 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int result = calculator.divide(15, 3);
         assertEquals(5, result, "15 / 3 doit être égal à 5");}
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void testAddParameterized(int a, int b, int expectedResult) {
+        assertEquals(expectedResult, Calculator.add(a, b));
     }
+
+
+
+
+}
+
+
